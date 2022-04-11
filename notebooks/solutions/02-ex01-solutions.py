b8dcbe2f279c9664362ae375d436cbc6bb65a93b
Thomas J. Fan <thomasjpfan@gmail.com>
2022-04-10 20:23:13 -0400

    ENH Update slides and notebooks

--- notebooks/solutions/02-ex01-solutions.py
+++ notebooks/solutions/02-ex01-solutions.py
@@ -1,4 +1,4 @@
-over_rf = make_imb_pipeline(RandomOverSampler(), RandomForestClassifier(random_state=42))
+over_rf = make_imb_pipeline(RandomOverSampler(random_state=0), RandomForestClassifier(random_state=42))
 
 base_rf_metrics
 
